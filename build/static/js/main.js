a7a063965026dc80b543caf1a133a544e4aaddbe
PavelSedoy <pavliksedoy@gmail.com>
2019-10-14 16:00:41 +0300

    About and Clients

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Home header slider
     var mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
         loop: true,
@@ -13,4 +14,21 @@ $(document).ready(function () {
           type: 'fraction',
         },
     });
+
+    // Clients slider
+    if ( $('.section-clients__swiper-slide').length > 6 ) {
+        $('.section-clients__swiper-slide').each( function () {
+            $(this).css('flex-basis', 'auto');
+        });
+        var mySwiperClients = new Swiper('.section-clients__swiper-container', {
+            // Optional parameters
+            loop: true,
+            slidesPerView: 6,
+            speed: 3000,
+
+            autoplay: {
+                delay: 2000,
+            },
+        });
+    }
 });
